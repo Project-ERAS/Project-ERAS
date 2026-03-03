56a73b4925c1dc2a1375cbc10795c1285c17b188
chanith <chanith321@gmail.com>
2026-03-03 12:18:29 +0530

    refactor: Redirected the splash to home page

--- app/splash.tsx
+++ app/splash.tsx
@@ -9,7 +9,7 @@ export default function SplashScreen() {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.push('/signin');
+      router.push('/(tabs)');
     }, 7000);
 
     return () => clearTimeout(timer);
