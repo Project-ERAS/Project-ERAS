4e7b0ec0593fad2e84afeb035d83f14e6e133b06
Loki-lan <alokailangage.com@gmail.com>
2026-03-15 15:36:20 +0530

    feat(firebase): add firebase config and app initialization

---
+++ constants/firebase.ts
@@ -0,0 +1,13 @@
+import { getApps, initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqdQlv4tY3L1npqYqXsYQHhLzf9O-8ink",
+  authDomain: "eras-cfa09.firebaseapp.com",
+  projectId: "eras-cfa09",
+  storageBucket: "eras-cfa09.firebasestorage.app",
+  messagingSenderId: "318087849877",
+  appId: "1:318087849877:web:49a4b74765c976081d789c",
+  measurementId: "G-JNLJNVLBCJ",
+};
+
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
